4b5214d24c1eef855c890b9322939755450ba6a2
sasta-kro <sai.aike.shwe.tun.aung@gmail.com>
2025-11-25 22:43:19 +0700

    bugs discovery

--- composeApp/src/webMain/kotlin/me/sasta/spendings_processor_cmp/main.kt
+++ composeApp/src/webMain/kotlin/me/sasta/spendings_processor_cmp/main.kt
@@ -9,3 +9,12 @@ fun main() {
         App()
     }
 }
+
+
+// TODO: give which date when having an 'invalid date format' error
+// TODO: when giving invalid month error, point to which line the error is located in like the year error message
+// TODO: program don't process last day when it is not padded with an empty line
+/* TODO: sometime when 2 individual comments are on consecutive lines, it gives invalid date error
+(prob because it is thinking that is an empty line and the next day has started)
+ */
+// TODO: sample disappears when typing, so i should have a permanently displaying sample
